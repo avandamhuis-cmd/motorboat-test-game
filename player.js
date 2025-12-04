59f5af27f94f9384f9bee1dd09af64b6c21ff55d
avandamhuis-cmd <avandamhuis@gmail.com>
2025-12-04 12:28:29 +0100

    Add speed properties for land and water movement

--- player.js
+++ player.js
@@ -3,9 +3,18 @@ const player = {
   x: 1500,
   y: 1500,
   r: 20,
+  speedLand: 4,
+  speedWater: 2, // slower movement
   speed: 4,
 
   update() {
+    // Check if in water
+    if (isPlayerInWater(this)) {
+      this.speed = this.speedWater;
+    } else {
+      this.speed = this.speedLand;
+    }
+
     if (keys['w']) this.y -= this.speed;
     if (keys['s']) this.y += this.speed;
     if (keys['a']) this.x -= this.speed;
@@ -16,13 +25,13 @@ const player = {
     const sx = this.x - camera.x;
     const sy = this.y - camera.y;
 
-    // Outer darker skin outline
+    // Outer edge
     ctx.fillStyle = '#a67c52';
     ctx.beginPath();
     ctx.arc(sx, sy, this.r, 0, Math.PI * 2);
     ctx.fill();
 
-    // Inner skin color
+    // Inner skin
     ctx.fillStyle = '#ffdbac';
     ctx.beginPath();
     ctx.arc(sx, sy, this.r - 3, 0, Math.PI * 2);
