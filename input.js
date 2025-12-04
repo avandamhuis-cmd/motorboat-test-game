ae38ec8f2e281d597fab6f07933bb14c64799dde
avandamhuis-cmd <avandamhuis@gmail.com>
2025-12-04 12:18:35 +0100

    Add WASD input handling

---
+++ input.js
@@ -0,0 +1,4 @@
+// Handles WASD input
+const keys = {};
+window.addEventListener('keydown', e => keys[e.key.toLowerCase()] = true);
+window.addEventListener('keyup', e => keys[e.key.toLowerCase()] = false);
